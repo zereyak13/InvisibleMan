f2660f0fc8573e1ae0fb0d3f8bd6d60dc50e1d48
zereyak13 <47759320+zereyak13@users.noreply.github.com>
2021-07-28 16:32:18 +0300

    Added: BasicPlayerMovement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private const float MOVE_SPEED = 5f;
+    private const float ROTATION_SPEED = 0.15f;
+
+
+    private StateHandler stateHandler;
+    private StateHandler.PlayerState playerState;
+    private Animator playerAnimator;
+
+    private Rigidbody playerRB;
+    private Vector3 moveDir;
+   
+    void Start()
+    {
+        playerRB = gameObject.GetComponent<Rigidbody>();
+        stateHandler = gameObject.GetComponent<StateHandler>();
+        playerAnimator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        PlayerInputs();
+        SetPlayerAnim();
+
+
+    }
+    private void FixedUpdate()
+    {
+        if (stateHandler.playerCurState == StateHandler.PlayerState.Running)
+        {
+            playerRB.MovePosition(transform.position + moveDir * MOVE_SPEED * Time.deltaTime);
+            //transform.rotation = Quaternion.LookRotation(moveDir);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDir), ROTATION_SPEED);
+            //playerRB.velocity = moveDir * MOVE_SPEED;
+        }     
+    }
+
+
+    private void PlayerInputs()
+    {
+        float horInput = Input.GetAxisRaw("Horizontal");
+        float verInput = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector3(horInput, 0, verInput).normalized;
+        if(moveDir.magnitude != 0)
+        {
+            stateHandler.playerCurState = StateHandler.PlayerState.Running;
+        }
+        else
+        {
+            stateHandler.playerCurState = StateHandler.PlayerState.Idle;
+            playerRB.velocity = Vector3.zero;
+        }
+
+    }
+    private void SetPlayerAnim()
+    {
+        switch (stateHandler.playerCurState)
+        {
+            case StateHandler.PlayerState.Idle:
+                //playerAnimator.set
+                playerAnimator.SetBool("running", false);
+                break;
+            case StateHandler.PlayerState.Running:
+                playerAnimator.SetBool("running", true);
+                break;
+            case StateHandler.PlayerState.Bath:
+                playerAnimator.SetBool("running", false);
+                playerAnimator.SetTrigger("bath");
+                break;
+            case StateHandler.PlayerState.Dead:
+                playerAnimator.SetTrigger("dead");
+                break;
+        }
+    } 
+}
---
+++ Assets/StateHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateHandler : MonoBehaviour
+{
+    public enum  PlayerState
+    {
+        Idle,
+        Running,
+        Bath,
+        Dead
+    }
+    public PlayerState playerCurState;
+
+
+
+    private void Start()
+    {
+        playerCurState = PlayerState.Idle;
+    }
+
+
+    void Update()
+    {
+        //Debug.Log(PlayerState.Idle);    
+    }
+    public void SetPlayerState(int StateID)
+    {
+        switch (StateID)
+        {
+            case (int)PlayerState.Idle:
+                playerCurState = PlayerState.Idle;
+                break;
+            case (int)PlayerState.Running:
+                playerCurState = PlayerState.Running;
+                break;
+            case (int)PlayerState.Bath:
+                playerCurState = PlayerState.Running;
+                break;
+        }
+    }
+}
