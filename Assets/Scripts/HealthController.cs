62a1d491d8dadddff888f22ed1b010e31c94388d
zereyak13 <47759320+zereyak13@users.noreply.github.com>
2021-08-16 09:02:38 +0300

    Added: Health Controller

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthController : MonoBehaviour
+{
+    public Image imageToFill;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.T))
+        {
+            imageToFill.fillAmount -= 0.01f;
+        }
+        else if (Input.GetKey(KeyCode.Y))
+        {
+            imageToFill.fillAmount += 0.01f;
+        }
+    }
+}
